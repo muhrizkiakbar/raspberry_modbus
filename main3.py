abf484a4c8be50b52f2988e2ff639d1ebd67dbde
muhrizkiakbar <muhrizkiakbar@live.com>
2025-06-14 12:59:26 +0800

    a

--- main3.py
+++ main3.py
@@ -1,6 +1,9 @@
 import serial
 import crcmod
 import time
+import Adafruit_SSD1306
+from PIL import Image, ImageDraw, ImageFont
+import textwrap
 
 # Konfigurasi Port Serial
 ser = serial.Serial(
@@ -14,6 +17,42 @@ ser = serial.Serial(
 
 # Fungsi CRC16 Modbus RTU
 crc16 = crcmod.mkCrcFun(0x18005, rev=True, initCrc=0xFFFF, xorOut=0x0000)
+
+OLED_WIDTH = 128
+OLED_HEIGHT = 64
+oled = Adafruit_SSD1306.SSD1306_128_64(rst=None)
+
+# Font Configuration
+font_path = "/home/pi/SMS/fonts/Tahoma.ttf"
+font = ImageFont.truetype(font_path, 11)
+
+
+def display_message(line1, line2=""):
+    """Display messages on OLED screen"""
+    try:
+        oled.begin()
+        oled.clear()
+        image = Image.new("1", (OLED_WIDTH, OLED_HEIGHT))
+        draw = ImageDraw.Draw(image)
+
+        char_width = font.getlength("A")
+        char_height = font.getbbox("A")[3]
+        max_chars_per_line = OLED_WIDTH // char_width
+
+        wrapped_text = textwrap.wrap(line1, width=max_chars_per_line)
+        total_text_height = len(wrapped_text) * char_height
+        y_offset = (OLED_HEIGHT - total_text_height) // 2
+
+        for line in wrapped_text:
+            text_width = font.getlength(line)
+            x_offset = (OLED_WIDTH - text_width) // 2
+            draw.text((x_offset, y_offset), line, font=font, fill=255)
+            y_offset += char_height
+
+        oled.image(image)
+        oled.display()
+    except Exception as e:
+        logging.error(f"OLED Display Error: {e}")
 
 
 def read_analog_channel(slave_address, channel_address):

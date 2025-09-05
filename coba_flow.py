bf5b4b1677e5fe10dcd0cd361625e5e7a907ebe8
muhrizkiakbar <muhrizkiakbar@live.com>
2025-09-05 14:16:21 +0800

    a

--- coba_flow.py
+++ coba_flow.py
@@ -21,7 +21,8 @@ def read_depth():
         # Hitung kedalaman
         # depth_mm = max(channel_height_mm - distance_mm, 0)
         # depth_mm = 65535 - (distance_mm - channel_height_mm)
-        depth_mm = channel_height_mm - distance_mm
+        # depth_mm = channel_height_mm - distance_mm
+        depth_mm = 65535 - (channel_height_mm + 100)
 
         return {
             "distance_mm": distance_mm,

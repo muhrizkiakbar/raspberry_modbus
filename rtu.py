1a1e9bbd4659bac9a824741916c23363625ecf34
muhrizkiakbar <muhrizkiakbar@live.com>
2025-09-06 14:01:03 +0800

    a

--- rtu.py
+++ rtu.py
@@ -220,7 +220,7 @@ class RTU:
                         sensor["name"]: {
                             "sensor_type": sensor["type"],
                             "unit": sensor.get("conversion", {}).get("unit", ""),
-                            "value": value,
+                            "value": round(float(value), 1),
                             "status": "OK" if value is not None else "error",
                         }
                     }
